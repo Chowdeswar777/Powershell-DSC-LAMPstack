2329a66b9fd46c3994587f0578efc64a57772aa7
vipin-k <vipinkumargupta@gmail.com>
2019-01-26 09:57:09 -0800

    v1.0
    few file

---
+++ LAMPservercentOS.ps1
@@ -0,0 +1,36 @@
+configuration LAMPServer {
+   Import-DSCResource -module "nx"
+
+   Node localhost {
+
+        $requiredPackages = @("httpd","mod_ssl","php","php-mysql","mariadb","mariadb-server")
+        $enabledServices = @("httpd","mariadb")
+
+        #Ensure packages are installed
+        ForEach ($package in $requiredPackages){
+            nxPackage $Package{
+                Ensure = "Present"
+                Name = $Package
+                PackageManager = "yum"
+            }
+        }
+
+			#Ensure php info files there
+		    nxFile phpinfophp{ 
+                DestinationPath = "/var/www/html/phpinfo.php"
+				Contents="<?php phpinfo(); ?>"
+				Type = "file"          
+                Ensure = "Present"
+            }
+
+        #Ensure daemons are enabled
+        ForEach ($service in $enabledServices){
+            nxService $service{
+                Enabled = $true
+                Name = $service
+                Controller = "SystemD"
+                State = "running"
+            }
+        }
+   }
+}
